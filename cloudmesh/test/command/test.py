612650d6b0057ed09b2c58bb56f4838c73ad2a26
Gregor von Laszewski <laszewski@gmail.com>
2020-02-21 07:00:44 -0500

    update manual page

--- cloudmesh/test/command/test.py
+++ cloudmesh/test/command/test.py
@@ -34,17 +34,16 @@ class TestCommand(PluginCommand):
              3. is cl installed
              4. is nmake installed
              5. is the username without spaces
-
-
-          Checks that are missing or need implemented
-
              6. are you running in a vnenv
              7. is the default mongo port used
              8. do you have docker installed
              9. do you have vbox installed
-            10. is hyperv switched on or off
-            11. how much memory do you have
-            12. do you have free diskspace
+            10. how much memory do you have
+            11. do you have free diskspace
+
+          Checks that are missing or need implemented
+
+            12. is hyperv switched on or off
             13. are containers running
             14. .... other tyings that can help us debug your environment
 
